c3bc57e94abd16df5b9efb211677bed128eea5b9
Kyle Tan <kyle@kyletan.com>
2024-09-08 11:44:59 -0700

    Keep floor selector close to map even on Desktop

--- src/components/FloorSelector.tsx
+++ src/components/FloorSelector.tsx
@@ -11,7 +11,7 @@ const FloorSelector: React.FC<FloorSelectorProps> = ({ floors, currentFloor, nex
   const sortedFloors = [...floors].sort((a, b) => b - a); // Sort floors in descending order
 
   return (
-    <div className="fixed left-4 top-1/2 transform -translate-y-1/2 bg-white shadow-lg rounded-full p-2">
+    <div className="absolute left-4 md:left-1/2 top-1/2 transform md:-translate-x-[21rem] -translate-y-1/2 bg-white shadow-lg rounded-full p-2">
       <div className="flex flex-col items-center space-y-2">
         {sortedFloors.map((floor) => (
           <div key={floor} className="relative">
